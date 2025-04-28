4badb4c1739b0aa30af9c71af1d8151cc0426699
Nagendra174 <bejugamnagendra2002@gmail.com>
2025-04-28 22:25:47 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Navbar from './navbar.jsx'
+import Hero from './hero.jsx'
+import Hero1 from './hero1.jsx'
+import Info from './info.jsx'
+import Grid from './grid.jsx'
+import Key from './key.jsx'
+import RealStories from "./RealStories.jsx";
+import Footer from './Footer.jsx'
+
+
+const App = () => {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        <Hero1 />
+        <Info />
+        <Grid />
+        <Key />
+        <RealStories />
+        <Footer />
+
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ src/navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className="flex justify-between items-center py-4 sm:mx-16">
+      <p className="font-matter-regular text-4xl tracking-tight">
+        Cogni<span className="font-matter-medium ">AI</span>
+      </p>
+      <div className="bg-stone-50 rounded-full flex items-center max-[1050px]:hidden ">
+        <button className="text-lg text-white px-9 bg-[#f46800] rounded-full py-3 font-matter-regular">
+          Home
+        </button>
+        <button className="text-lg text-stone-700  px-7 font-matter-medium">
+          Work
+        </button>
+        <button className="text-lg text-stone-700  px-7 font-matter-medium">
+          Expertise
+        </button>
+        <button className="text-lg text-stone-700  px-7 font-matter-medium">
+          Pricing
+        </button>
+        <button className="text-lg text-stone-700  px-7 font-matter-medium">
+          Testimonial
+        </button>
+      </div>
+      <button className="text-lg py-3 px-9 bg-black text-white rounded-full font-matter-light">
+        Contact Us
+      </button>
+    </div>
+  )
+}
+
+export default navbar
