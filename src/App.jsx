479811df63212c876e136701d4f53600996f2a68
Nagendra174 <bejugamnagendra2002@gmail.com>
2025-04-28 22:31:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from './navbar.jsx'
 import Hero from './hero.jsx'
 import Hero1 from './hero1.jsx'
-import Info from './info.jsx'
 import Grid from './grid.jsx'
 import Key from './key.jsx'
 import RealStories from "./RealStories.jsx";
